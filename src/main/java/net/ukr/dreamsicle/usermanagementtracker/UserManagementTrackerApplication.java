4acc80583182a63039d407521c39da4a8706bb5c
yurii.loienko <yurii.loienko@sigma.software>
2020-09-14 17:28:59 +0300

    Initial commit

---
+++ src/main/java/net/ukr/dreamsicle/usermanagementtracker/UserManagementTrackerApplication.java
@@ -0,0 +1,13 @@
+package net.ukr.dreamsicle.usermanagementtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ukr/dreamsicle/usermanagementtracker/UserManagementTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ukr.dreamsicle.usermanagementtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
